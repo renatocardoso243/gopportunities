39ce015725168b82d14c62b5a01f582362960856
Renato Cardoso <renatoscardoso77@gmail.com>
2024-11-12 11:32:18 -0400

    feat: add documentation for create opening

--- handler/response.go
+++ handler/response.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/renatocardoso243/gopportunities.git/schemas"
 )
 
 func sendError(ctx *gin.Context, code int, msg string) {
@@ -20,4 +21,14 @@ func sendSuccess(ctx *gin.Context, op string, data interface{}) {
 	ctx.JSON(http.StatusOK, gin.H{
 		"message": fmt.Sprintf("operation from handler:  %s successfull", op), 
 		"data": data,})
+}
+
+type ErrorResponse struct {
+	Message 	string `json:"message"`
+	ErrorCode 	string `json:"errorCode"`
+}
+
+type CreateOpeningResponse struct {
+	Message string `json:"message"`
+	Data 	schemas.OpeningResponse `json:"data"`
 }
