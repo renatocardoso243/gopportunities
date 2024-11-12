86e5808a2f7fc4b9a601888f215eb2e8d52f876f
Renato Cardoso <renatoscardoso77@gmail.com>
2024-11-12 09:44:19 -0400

    feat: add update request logic

--- handler/request.go
+++ handler/request.go
@@ -40,3 +40,23 @@ func (r *CreateOpeningRequest) Validate() error {
 	}
 	return nil
 }
+
+//UpdateOpening
+
+type UpdateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *UpdateOpeningRequest) Validate() error {
+	//if any field provided, validation is true
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Remote != nil || r.Link != "" || r.Salary > 0 {
+		return nil
+	}
+	// If none of fields were provided, return falsy
+	return fmt.Errorf("at least one valid field must be provided")
+}
